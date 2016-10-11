68a7e1f8e3f66144fafa3a4bd197e44a357f6861
nikepupu <gongran911@163.com>
2016-10-11 01:17:01 -0700

    add sidebar features

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,11 @@
 
 $(function() {
     var offset = $("#sidebar").offset();
-    var topPadding = 15;
+    
     $(window).scroll(function() {
         if ($(window).scrollTop() > offset.top) {
             $("#sidebar").stop().animate({
-                marginTop: $(window).scrollTop() - offset.top + topPadding
-            });
+                marginTop: $(window).scrollTop() - offset.top });
         } else {
             $("#sidebar").stop().animate({
                 marginTop: 0
